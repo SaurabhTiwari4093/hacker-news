13c88fdc1809d35123e5794d57b4be5d681f9838
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-01-16 22:40:58 +0530

    Updated

--- src/components/NewsCompSearch.js
+++ src/components/NewsCompSearch.js
@@ -40,18 +40,39 @@ export default function NewsComp(props) {
         updateNews()
     }, [])
 
-    const callSearchApi=(query)=>{
+    const callSearchApi = (query) => {
         setSearchQuery(query);
         updateNews()
     }
 
     return (
         <>
-            <div className="container input-group mb-4" style={{ marginTop: '100px', paddingRight: 30, paddingLeft: 30 }}>
-                <span className="input-group-text">
+            <div className="container input-group" style={{ marginTop: '100px', paddingRight: 30, paddingLeft: 30 }}>
+                <span className={`input-group-text bg-${props.mode}`}>
                     <img src={SearchIcon} alt="search-icon" height={20} width={20} />
                 </span>
-                <input className="form-control" placeholder="Search" type="search" aria-label="Search" onChange={(e) => callSearchApi(e.target.value)} />
+                <input className={`form-control bg-${props.mode}`} placeholder="Search" type="search" aria-label="Search" onChange={(e) => callSearchApi(e.target.value)} />
+            </div>
+            <div className="container input-group my-4" style={{ paddingRight: 30, paddingLeft: 30 }}>
+                <span className={`input-group-text bg-${props.mode}`}>Search</span>
+                <select className={`form-select bg-${props.mode}`} aria-label="All">
+                    <option selected value="All">All</option>
+                    <option value="Stories">Stories</option>
+                    <option value="Comments">Comments</option>
+                </select>
+                <span className={`input-group-text bg-${props.mode}`}>by</span>
+                <select className={`form-select bg-${props.mode}`} aria-label="Popularity">
+                    <option selected value="Popularity">Popularity</option>
+                    <option value="Date">Date</option>
+                </select>
+                <span className={`input-group-text bg-${props.mode}`}>for</span>
+                <select className={`form-select bg-${props.mode}`} aria-label="All time">
+                    <option selected value="AllTime">All Time</option>
+                    <option value="Last24h">Last 24h</option>
+                    <option value="PastWeek">Past Week</option>
+                    <option value="PastMonth">Past Month</option>
+                    <option value="PastYear">Past Year</option>
+                </select>
             </div>
             <InfiniteScroll
                 dataLength={articles.length}
